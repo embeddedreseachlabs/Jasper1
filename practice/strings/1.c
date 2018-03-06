abdc927c7af4d4ca7a2d938c52b95643315471d5
Pushlatha <pushpalathad36@gmail.com>
2018-03-06 00:38:04 +0000

    My first program on Strings

---
+++ practice/strings/1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+char arr[ ] = "surprised" ;
+char *ptr = "surprised" ;
+printf(" string fetched by array is %s\n",arr);
+printf("string fetched by pointer is %s\n",ptr);
+} 
